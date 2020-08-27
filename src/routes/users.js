257ecca40721efc9a3603a05c0f97db5446a7825
Felipe <felipedemelopontes@gmail.com>
2020-08-27 14:25:07 +0200

    feat: sessionController

---
+++ src/routes/users.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const routes = express.Router()
+
+const HomeController = require('../app/controllers/SessionController')
+const UserController = require('../app/controllers/UserController')
+
+// routes.get('/login', SessionController.loginForm)
+// routes.post('/login', SessionController.login)
+// routes.post('/logout', SessionController.logout)
+
+// routes.get('/forgot-password', SessionController.forgotForm)
+// routes.get('/password-reset', SessionController.resetForm)
+// routes.post('/forgot-password', SessionController.forgot)
+// routes.post('/password-reset', SessionController.reset)
+
+// routes.get('/register', UserController.registerForm)
+// routes.post('/register', UserController.post)
+
+// routes.get('/', UserController.show)
+// routes.put('/', UserController.update)
+// routes.get('/', UserController.delete)
+
+module.exports = routes
